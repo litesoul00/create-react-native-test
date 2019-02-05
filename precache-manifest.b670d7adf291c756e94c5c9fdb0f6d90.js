1be0209b423b3139de3debc84abc1f00dfea878f
litesoul00 <jasons8002@gmail.com>
2019-02-05 02:16:16 -0800

    Updates

---
+++ precache-manifest.b670d7adf291c756e94c5c9fdb0f6d90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/litesoul00/parkzoness/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "76c3683fc432c1d7522e",
+    "url": "/litesoul00/parkzoness/static/js/runtime~main.76c3683f.js"
+  },
+  {
+    "revision": "4802da4261cd640eab30",
+    "url": "/litesoul00/parkzoness/static/js/main.4802da42.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/litesoul00/parkzoness/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "4802da4261cd640eab30",
+    "url": "/litesoul00/parkzoness/static/css/main.f7ae7baa.chunk.css"
+  },
+  {
+    "revision": "bee48e5b3d1b63f1dc8e9156be5936e8",
+    "url": "/litesoul00/parkzoness/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/litesoul00/parkzoness/precache-manifest.b670d7adf291c756e94c5c9fdb0f6d90.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/litesoul00/parkzoness/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
